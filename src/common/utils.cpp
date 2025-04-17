0503997598a1f312d4c8242ea35856f96379d9e1
AlexPeshkoff <alexander.peshkoff@gmail.com>
2025-04-17 17:24:05 +0300

    Postfix for #8520: Error in iTransaction.getInfo() on embedded connection; isc_info_end should be present in the end
    (cherry picked from commit cf1d3672dba5b159da6b7cf202b877cc3cbd82fe)

--- src/common/utils.cpp
+++ src/common/utils.cpp
@@ -1583,9 +1583,9 @@ void getDbPathInfo(unsigned int& itemsLength, const unsigned char*& items,
 			--itemsLength;
 
 			unsigned int len = dbpath.length();
-			if (len + 3 > bufferLength)
+			if (len + 4 > bufferLength)
 			{
-				len = bufferLength - 3;
+				len = bufferLength - 4;
 			}
 			bufferLength -= (len + 3);
 			*buffer++ = fb_info_tra_dbpath;
@@ -1593,6 +1593,7 @@ void getDbPathInfo(unsigned int& itemsLength, const unsigned char*& items,
 			*buffer++ = len >> 8;
 			memcpy(buffer, dbpath.c_str(), len);
 			buffer += len;
+			*buffer = isc_info_end;
 		}
 	}
 }
