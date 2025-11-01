17610be99d770a5d605a309d03070e6b57b4badd
Vlad Khorsun <hvlad@users.sourceforge.net>
2025-11-01 12:48:24 +0200

    Fixed bug #8794 : Called procedure parameter changes during execution without assignment when deleting from updatable view in Firebird 5

--- src/dsql/StmtNodes.cpp
+++ src/dsql/StmtNodes.cpp
@@ -2607,6 +2607,8 @@ EraseNode* EraseNode::pass2(thread_db* tdbb, CompilerScratch* csb)
 
 	csb->csb_rpt[stream].csb_flags |= csb_update;
 
+	impureOffset = csb->allocImpure<impure_state>();
+
 	return this;
 }
 
@@ -2667,6 +2669,8 @@ const StmtNode* EraseNode::erase(thread_db* tdbb, Request* request, WhichTrigger
 	{
 		case Request::req_evaluate:
 		{
+			impure->sta_state = 0;
+
 			if (!(marks & MARK_AVOID_COUNTERS))
 				request->req_records_affected.bumpModified(false);
 
