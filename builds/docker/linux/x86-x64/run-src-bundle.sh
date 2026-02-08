e450e2d74e141542d3730b8d353310b5c49249ee
Adriano dos Santos Fernandes <adrianosf@gmail.com>
2026-02-08 18:13:16 -0300

    Fix src-bundle job

--- builds/docker/linux/x86-x64/run-src-bundle.sh
+++ builds/docker/linux/x86-x64/run-src-bundle.sh
@@ -3,4 +3,6 @@ docker run --platform amd64 --rm \
 	-e CTNG_UID=${CTNG_UID:-$(id -u)} \
 	-e CTNG_GID=${CTNG_GID:-$(id -g)} \
 	-v `pwd`/../../../..:/firebird \
-	-t --entrypoint /entry-src-bundle.sh asfernandes/firebird-builder:fb6-x64-ng-v1
+	-v `pwd`/../../../../gen:/home/ctng/firebird-build/gen \
+	--entrypoint /entry-src-bundle.sh \
+	-t firebirdsql/firebird-builder-linux:fb6-x64-ng-v2
