1a921a13511b40b45d8338dbf917e3671be490bb
Adriano dos Santos Fernandes <adrianosf@gmail.com>
2026-02-12 19:21:35 -0300

    Use read-only source mounts in Linux docker builds

--- builds/docker/linux/x86-x64/run-x86.sh
+++ builds/docker/linux/x86-x64/run-x86.sh
@@ -3,6 +3,6 @@ mkdir -p `pwd`/../../../../gen
 docker run --platform i386 --rm \
 	-e CTNG_UID=${CTNG_UID:-$(id -u)} \
 	-e CTNG_GID=${CTNG_GID:-$(id -g)} \
-	-v `pwd`/../../../..:/firebird \
+	-v `pwd`/../../../..:/firebird:ro \
 	-v `pwd`/../../../../gen:/home/ctng/firebird-build/gen \
 	-t firebirdsql/firebird-builder-linux:fb6-x86-ng-v2
